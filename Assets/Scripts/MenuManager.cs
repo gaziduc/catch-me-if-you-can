5b720fb5ff9d975a0d4567639ab0e65b49e271d7
KoHann <alexandre.vermeulen@laposte.net>
2021-02-06 18:13:19 +0100

    Add level 2 and some features

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,21 +5,25 @@ public class MenuManager : MonoBehaviour
 {
     public void LoadScene(int sceneBuildIndex)
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(sceneBuildIndex);
     }
     
     public void LoadSceneByName(string sceneName)
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(sceneName);
     }
     
     public void LoadNextScene()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void ReloadScene()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
     
