5dd0f5d743fed50df37796ac2b213724158b3735
David Ghiassi <david.ghiassi@gmail.com>
2021-02-07 11:36:29 +0100

    Smooth camera movement done

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,12 +4,20 @@ public class CameraFollow : MonoBehaviour
 {
     public Vector2 minPosition;
     public Vector2 maxPosition;
+
+    private Vector2 velocity;
+
+    public float smoothTime;
+    
     
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Camera.main.transform.position = new Vector3(Mathf.Clamp(transform.position.x, minPosition.x, maxPosition.x),
-                                                        Mathf.Clamp(transform.position.y, minPosition.y, maxPosition.y),
+        float posX = Mathf.SmoothDamp(Camera.main.transform.position.x, transform.position.x, ref velocity.x, smoothTime);
+        float posY = Mathf.SmoothDamp(Camera.main.transform.position.y, transform.position.y, ref velocity.y, smoothTime);
+        
+        Camera.main.transform.position = new Vector3(Mathf.Clamp(posX, minPosition.x, maxPosition.x),
+                                                        Mathf.Clamp(posY, minPosition.y, maxPosition.y),
                                                         Camera.main.transform.position.z);
     }
 }
