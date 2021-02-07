870718b841ac605d19f1b80dc443fa45557534e3
David Ghiassi <david.ghiassi@gmail.com>
2021-02-07 23:15:24 +0100

    Added level selector

--- Assets/Scripts/PanelOpener.cs
+++ Assets/Scripts/PanelOpener.cs
@@ -4,13 +4,27 @@ public class PanelOpener : MonoBehaviour
 {
     public GameObject settingsPanel;
     public GameObject helpPanel;
-
+    public GameObject levelsPanel;
 
     public void OpenSettingsPanel()
     {
         if (settingsPanel != null)
         {
             Animator anim = settingsPanel.GetComponent<Animator>();
+
+            if (anim != null)
+            {
+                bool isOpen = anim.GetBool("Open");
+                anim.SetBool("Open", !isOpen);
+            }
+        }
+    }
+    
+    public void OpenLevelsPanel()
+    {
+        if (levelsPanel != null)
+        {
+            Animator anim = levelsPanel.GetComponent<Animator>();
 
             if (anim != null)
             {
