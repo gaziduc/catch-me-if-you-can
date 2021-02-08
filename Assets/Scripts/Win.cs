e82642c1031822268d64184c799d64aa09a4f388
David Ghiassi <david.ghiassi@gmail.com>
2021-02-08 22:22:15 +0100

    Added score on win panel

--- Assets/Scripts/Win.cs
+++ Assets/Scripts/Win.cs
@@ -2,16 +2,20 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Win : MonoBehaviour
 {
     public GameObject endScreen;
+    public Text scoreText;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             endScreen.SetActive(true);
+            Inventory inventory = other.gameObject.GetComponent<Inventory>();
+            scoreText.text = "SCORE: " + ((inventory.numKills + inventory.numTVs) * 100).ToString();
             other.gameObject.GetComponent<PlayerMove>().canMove = false;
             Time.timeScale = 0f;
         }
